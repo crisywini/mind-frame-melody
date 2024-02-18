fc15ccd6a48815d843f47980b9e51b508854ad49
Cristian Sanchez Pineda <cristian.sanchezp@perficient.com>
2024-02-18 18:28:11 -0500

    Added main template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project that analyze different multi-media depression data and aims to classify it',
+    author='Cristian Sánchez Pineda',
+    license='MIT',
+)
